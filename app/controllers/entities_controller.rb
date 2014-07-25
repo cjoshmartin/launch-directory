836646e9c0ab28eef3d21ad7cf4509c5e1a00452
Josh Martin <joshmartin@gmail.com>
2014-07-25 09:50:12 -0400

    We got form working

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -3,6 +3,16 @@ class EntitiesController < ApplicationController
     @entities =Entity.all
   end
   def new
-    
+    @entity= Entity.new
+  end
+  def create
+    @entity = Entity.new(entity_params)
+    if @entity.save
+      redirect_to :action => :index
+
+    end
+  end
+  def entity_params
+    params.require(:entity).permit(:name,:twitter, :description)
   end
 end
